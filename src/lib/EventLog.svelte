9c96d31c5c35584015ac90aa1ead355b556af03d
Darrel O'Pry <darrel.opry@spry-group.com>
2025-11-01 23:38:37 -0400

    feat!: migrate to svelte5 + sveltkit
    - rebuilt on sv create with library option

--- src/lib/EventLog.svelte
+++ src/lib/EventLog.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
 
+import type { User, UserManager } from 'oidc-client-ts';
 import { getContext, onDestroy, onMount } from 'svelte';
 import { OIDC_CONTEXT_CLIENT_PROMISE } from './OidcContext.svelte';
-import { User, UserManager } from 'oidc-client-ts';
 
 let logs: string[] = [];
-const log = (message: string) => {
-  logs = [...logs, message];
-};
-
-let accessTokenExpiredHandler = () => log('Access token expired');
-let accessTokenExpiringHandler = () => log('Access token expiring');
-let silentRenewErrorHandler = (e: Error) => log(`SilentRenewError: ${e.message}`);
-let userLoadedHandler = (user: User) => log(`User loaded: ${JSON.stringify(user)}`);
-let userSessionChangedHandler = () => log('User session changed');
-let userSignedInHandler = () => log(`User signed in`);
-let userSignedOutHandler = () => log('User signed out');
-let userUnloadedHandler = () => log('User unloaded');
 let userManager: UserManager | undefined = undefined;
+let userLoadedHandler: (user: User) => void;
+let userUnloadedHandler: () => void;
 
 // getContext must be called at the top level
 const oidcPromise = getContext<Promise<UserManager>>(OIDC_CONTEXT_CLIENT_PROMISE);
@@ -25,27 +15,20 @@ const oidcPromise = getContext<Promise<UserManager>>(OIDC_CONTEXT_CLIENT_PROMISE
 onMount(async () => {
   userManager = await oidcPromise;
   if (!userManager || !userManager.events) return;
-  userManager.events.addAccessTokenExpired(accessTokenExpiredHandler);
-  userManager.events.addAccessTokenExpiring(accessTokenExpiringHandler);
-  userManager.events.addSilentRenewError(silentRenewErrorHandler);
+  userLoadedHandler = (user) => {
+    logs = [...logs, `User logged in: ${JSON.stringify(user)}`];
+  };
+  userUnloadedHandler = () => {
+    logs = [...logs, 'User logged out'];
+  };
   userManager.events.addUserLoaded(userLoadedHandler);
-  userManager.events.addUserSessionChanged(userSessionChangedHandler);
-  userManager.events.addUserSignedIn(userSignedInHandler);
-  userManager.events.addUserSignedOut(userSignedOutHandler);
   userManager.events.addUserUnloaded(userUnloadedHandler);
-  userManager.revokeTokens
 });
 
 onDestroy(() => {
   if (userManager && userManager.events) {
-    userManager.events.removeAccessTokenExpired(accessTokenExpiredHandler);
-    userManager.events.removeAccessTokenExpiring(accessTokenExpiringHandler);
-    userManager.events.removeSilentRenewError(silentRenewErrorHandler);
-    userManager.events.removeUserLoaded(userLoadedHandler);
-    userManager.events.removeUserSessionChanged(userSessionChangedHandler);
-    userManager.events.removeUserSignedIn(userSignedInHandler);
-    userManager.events.removeUserSignedOut(userSignedOutHandler);
-    userManager.events.removeUserUnloaded(userUnloadedHandler);
+    if (userLoadedHandler) userManager.events.removeUserLoaded(userLoadedHandler);
+    if (userUnloadedHandler) userManager.events.removeUserUnloaded(userUnloadedHandler);
   }
 });
 </script>
@@ -53,7 +36,7 @@ onDestroy(() => {
 <div class="event-log">
   <h5>OIDC Event Log</h5>
   <ul>
-    {#each logs as log, i (i)}
+    {#each logs as log, i}
       <li>{log}</li>
     {/each}
   </ul>
